b5f882b3f83f096010067187db39b27744c5730f
Ana Ribeiro <anaarsoares@gmail.com>
2024-04-18 17:23:01 +0100

    Added more weather data functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,37 @@ function refreshWeather(response) {
     temperatureElement= document.querySelector("#temperature");
    let temperature = response.data.temperature.current;
    let cityElement = document.querySelector("#city");
-   cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(temperature);
+cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(temperature);
+
+ let descriptionElement = document.querySelector("#description");
+ let description = response.data.condition.description;
+ descriptionElement.innerHTML = description;
+
+ let humidityElement = document.querySelector("#humidity");
+ let humidity = response.data.temperature.humidity;
+ humidityElement.innerHTML = `${Math.round(humidity)}%`;
+
+ let windElement = document.querySelector("#wind");
+ let wind = response.data.wind.speed;
+ windElement.innerHTML = `${Math.round(wind)}km/h`;
+
+ let date= new Date(response.data.time * 1000);
+ let timeElement = document.querySelector("#time");
+ timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hour = date.getHours();
+    let days = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes<10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day}, ${hour}:${minutes},`
 }
 
 
