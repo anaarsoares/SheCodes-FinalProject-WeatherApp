7cda13943174d14f873c1e3c9c47d405e82f0faf
Ana Ribeiro <anaarsoares@gmail.com>
2024-04-18 12:11:30 +0100

    added axios

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ function handleSearchSubmit(event){
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value; 
 
-
 }
-
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
