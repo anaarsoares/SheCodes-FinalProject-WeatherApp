d10299fc0ade04fa295307aeb3078688b166c5fc
Ana Ribeiro <anaarsoares@gmail.com>
2024-04-18 12:41:17 +0100

    Search function using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+    temperatureElement= document.querySelector("#temperature");
+   let temperature = response.data.temperature.current;
+   let cityElement = document.querySelector("#city");
+   cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city){
+let apiKey = "279o37234fc0ba3effba9a107a3601tc";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value; 
-
+    let searchInput = document.querySelector("#search-form-input"); 
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("Porto");
